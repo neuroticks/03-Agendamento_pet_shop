dfde8ab4346a58bd37dc1682798bd20f584a58d7
Rodrigo Ferraro <neuroticks@gmail.com>
2025-05-13 01:03:13 +0000

    acho que refresh fica um nome melhor

--- src/modules/schedules/refresh.js
+++ src/modules/schedules/refresh.js
@@ -6,8 +6,8 @@ import { schedulesDay } from "./load"
 const selectedDate = document.getElementById("date")
 
 selectedDate.onchange = () => {
-    console.log("file schedules/list.js");
     console.log("Atualiza a tela com os agendamentos conforme a data selecionada");
+    console.log("file schedules/list.js");
 
     schedulesDay()
 }
