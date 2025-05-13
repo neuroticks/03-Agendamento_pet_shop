65d85b9718c0421dd18f4874ebda7a7dcd72f7c2
Rodrigo Ferraro <neuroticks@gmail.com>
2025-05-13 15:31:43 -0300

    mantem a mascara enquanto digita o numero do telefone

--- src/modules/form/open-close.js
+++ src/modules/form/open-close.js
@@ -1,4 +1,4 @@
-import dayjs from "dayjs"
+import dayjs from "../../libs/dayjs"
 import { loadSelectHoursAvailable } from "./hours-load"
 
 const botaoNovoAgendamento = document.querySelector('.div-botao-agendar');
@@ -16,6 +16,7 @@ export function novoAgendamento() {
         telaPopUp.style.display = 'flex';
         telaListaAgendamentos.classList.add('blur');
         setDateTimeSelectValues();
+        mantemMascaraDigitandoFone();
     });
 }
 
@@ -33,6 +34,27 @@ async function setDateTimeSelectValues() {
 
     const hoje = dayjs().format("YYYY-MM-DD").toString()
     serviceDateSelect.value = hoje
+    serviceDateSelect.min = hoje
 
     await loadSelectHoursAvailable()
 }
+
+function mantemMascaraDigitandoFone() {
+    document.getElementById('phone').addEventListener('input', function (e) {
+        let valor = e.target.value.replace(/\D/g, ''); // Remove tudo que não é número
+        let valorFormatado = '';
+
+        if (valor.length > 0) {
+            valorFormatado = `(${valor.substring(0, 2)}`; // (XX
+        }
+        if (valor.length > 2) {
+            valorFormatado += `) ${valor.substring(2, 7)}`; // ) XXXXX
+        }
+        if (valor.length > 7) {
+            valorFormatado += `-${valor.substring(7, 11)}`; // -XXXX
+        }
+        
+        e.target.value = valorFormatado;
+    });
+}
+
