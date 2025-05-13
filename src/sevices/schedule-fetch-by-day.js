8c2b33f83e2d914119e9833800634b3b0b9151c1
Rodrigo Ferraro <neuroticks@gmail.com>
2025-05-13 03:31:28 +0000

    acrescentado alguns console.log pra entender o funcionamento

--- src/sevices/schedule-fetch-by-day.js
+++ src/sevices/schedule-fetch-by-day.js
@@ -1,15 +1,26 @@
 import dayjs from "dayjs"
 import { apiConfig } from "./api-config"
 
-export async function scheduleFetchByDay( { date } ) {
+export async function scheduleFetchByDay({ date }) {
     try {
+        // console.log(` === == == date = ${date}`)
         const response = await fetch(`${apiConfig.baseURL}/agendamentos`)
 
         const data = await response.json()
 
-        const dailySchedules = data.filter( (schedule) =>
+        // const teste = data.map((elem) =>
+        //     console.log(`### id=${elem.id} // when=${elem.when} // pet_name=${elem.pet_name} // name=${elem.name} // servico=${elem.servico}`)
+        // )
+
+        const dailySchedules = data.filter((schedule) =>
             dayjs(date).isSame(schedule.when, "day")
         )
+
+        // console.log(" ----- antes")
+        // const xpto = dailySchedules.map((elem) =>
+        //     console.log(`>>>>>> id=${elem.id} // when=${elem.when} // pet_name=${elem.pet_name} // name=${elem.name} // servico=${elem.servico}`)
+        // )
+        // console.log(" ----- depois")
 
         return dailySchedules
 
