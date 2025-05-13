56522946481a47c6f892045de270d83d1c799bd9
Rodrigo Ferraro <neuroticks@gmail.com>
2025-05-13 13:40:13 +0000

    remocao import nao usado

--- src/modules/form/date-change.js
+++ src/modules/form/date-change.js
@@ -1,7 +1,6 @@
 /***************************************************************
 Atualiza o select HORA com as horas disponíveis para agendamento
 */
-import { scheduleFetchByDay } from "../../sevices/schedule-fetch-by-day"
 import { loadSelectHoursAvailable } from "./hours-load"
 
 const serviceDateSelect = document.getElementById("service-date")
